c7e907af6a4b948e585741bfcd79d33fef6b351f
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:57:28 -0700

    Create main_19.ts

---
+++ excersie19/main_19.ts
@@ -0,0 +1,7 @@
+const gust_list: string [] = ["Ali", "Umer", "Maaz", "Rehan", "Hassan"];
+
+// gust_list.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me`));
+
+let lengthguests: number =gust_list.length;
+
+console.log(`We are Inviting total ${lengthguests} guests.`);
