dd45ab010aafb32f7eff3dc6bacbbfbe2b9cf45f
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:57:35 -0700

    Create main_20.ts

---
+++ excersie20/main_20.ts
@@ -0,0 +1,10 @@
+// Making a Programing Language Array
+let programingLanguage: string[] = ["Typescript","JavaScript","Python","PHP"];
+
+
+// Print the Massage of list
+console.log("List of Programing Language");
+
+
+// print the value of array in the form of list
+programingLanguage.forEach(Language => console.log(Language));
