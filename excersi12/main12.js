6b85ce2269bb0ccb8329676ba4ed78b451be5bb0
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:56:14 -0700

    Create main12.js

---
+++ excersi12/main12.js
@@ -0,0 +1,5 @@
+var friendsName = ["Ali", "Hussain", "Ahmed", "Yasir"];
+console.log('Hello ${friendsName}[0], how are you?');
+console.log('Hello ${friendsName}[1], how are you?');
+console.log('Hello ${friendsName}[2], how are you?');
+console.log('Hello ${friendsName}[3], how are you?');
