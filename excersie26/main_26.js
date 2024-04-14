b86c2f474ec62cdbc9f97751683662af02235061
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:59:21 -0700

    Create main_26.js

---
+++ excersie26/main_26.js
@@ -0,0 +1,13 @@
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
+if (alienColor === "whit") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
