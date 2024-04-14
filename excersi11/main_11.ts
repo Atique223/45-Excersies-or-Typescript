bc02d48d182a97f25a8beec154a25a45d0fc11eb
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:56:03 -0700

    Create main_11.ts

---
+++ excersi11/main_11.ts
@@ -0,0 +1,9 @@
+let friendeName: string[] = ["Ali",  "Hussain", "Ahmed", "Yasir"];
+            // index number   00        1           2       3
+
+console.log(friendeName[0]);
+console.log(friendeName[1]);
+console.log(friendeName[2]);
+console.log(friendeName[3]);
+
+ 
