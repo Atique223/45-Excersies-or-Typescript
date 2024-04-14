a77eb510c13f29b5bb91599c8c2b71ccbd1093b9
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:56:39 -0700

    Create main_15.js

---
+++ excersie15/main_15.js
@@ -0,0 +1,13 @@
+/*
+  You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations.
+  You’ll have to think of someone else to invite.
+• Start with your program from Exercise 14.
+Add a print statement at the end of your program stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.
+*/
+var gust_list = ["Ali", "Umer", "Maaz", "Rehan"];
+var dontcome = gust_list[1];
+console.log(dontcome, "you are not come on my dinner");
+gust_list.splice(1, 1, "Noor");
+gust_list.forEach(function (gust_list) { return console.log("Salam ".concat(gust_list, ", would you like to dinner with me?")); });
