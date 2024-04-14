ff8606a69b73a8c61412022696e4ccc12b426b92
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:59:15 -0700

    Create main_25.js

---
+++ excersie25/main_25.js
@@ -0,0 +1,7 @@
+var alienColor = "black";
+if (alienColor === "black") {
+    console.log("Alien color is black player just earned 5 points");
+}
+if (alienColor === "white") {
+    console.log("alien color is white");
+}
