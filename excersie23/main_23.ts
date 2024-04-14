c711b1bafec90d8583fa85cf9c51c4311412f5ae
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:57:53 -0700

    Create main_23.ts

---
+++ excersie23/main_23.ts
@@ -0,0 +1,44 @@
+
+let five = 5;
+
+let ten = 10;
+
+// test 1
+console.log("test 1: five is equal to 5?");
+console.log(five == 5);
+
+// test 2
+console.log("test 2: ten is equal to 10?");
+console.log(ten == 10);
+
+// test 3
+console.log("test 3: five is not equal to 10?");
+console.log(five != 10);
+
+// test 4
+console.log("test 4: ten is greater than 5?");
+console.log(ten > 5);
+
+// test 5/
+console.log("test 5: five is less than 10?");
+console.log(five < 10);
+
+// test 6
+console.log("\ntest 6: five is equal to 10?");
+console.log(five == 10);
+
+// test 7
+console.log("test 7: ten is equal to 5?");
+console.log(ten == 5);
+
+// test 8
+console.log("test 8: five is greater than 10?");
+console.log(five > 10);
+
+// test 9
+console.log("test 9: 10 is less than 5?");
+console.log(10 < 5);
+
+// test 10
+console.log("test 10: five is not equal to 5?");
+console.log(five != 5);
