c1db1c43c963940994bcac70a46ae860bfedd367
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:57:32 -0700

    Create main_20.js

---
+++ excersie20/main_20.js
@@ -0,0 +1,6 @@
+// Making a Programing Language Array
+var programingLanguage = ["Typescript", "JavaScript", "Python", "PHP"];
+// Print the Massage of list
+console.log("List of Programing Language");
+// print the value of array in the form of list
+programingLanguage.forEach(function (Language) { return console.log(Language); });
