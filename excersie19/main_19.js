bed60eb7dabd14ad9caa14ebf5d2582f5f00acb8
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:57:24 -0700

    Create main_19.js

---
+++ excersie19/main_19.js
@@ -0,0 +1,4 @@
+var gust_list = ["Ali", "Umer", "Maaz", "Rehan", "Hassan"];
+// gust_list.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me`));
+var lengthguests = gust_list.length;
+console.log("We are Inviting total ".concat(lengthguests, " guests."));
