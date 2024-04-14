91cc32499ef4b22fbef9fdbdf27fca95b4c0e6f3
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-14 07:56:09 -0700

    Create main_11.js

---
+++ excersi11/main_11.js
@@ -0,0 +1,6 @@
+var friendeName = ["Ali", "Hussain", "Ahmed", "Yasir"];
+// index number   00        1           2       3
+console.log(friendeName[0]);
+console.log(friendeName[1]);
+console.log(friendeName[2]);
+console.log(friendeName[3]);
