4043732660be254e36bcaab19ff94aba062d9555
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-28 08:47:57 -0700

    Create main_37.ts

---
+++ excersie37/main_37.ts
@@ -0,0 +1,10 @@
+function makeShirt (size: string = "Large", printMessage: string = "I Love TypeScript"){
+    console.log(`creating a ${size} shirt with the ${printMessage} prints on shirt`)
+
+}
+
+makeShirt();
+
+makeShirt("Medium");
+
+makeShirt("Small", "I Love IT");
