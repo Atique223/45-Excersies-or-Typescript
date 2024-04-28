c3df875f886454c634f60239df89c75914d6db5c
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-28 08:47:06 -0700

    Create main_29.ts

---
+++ excersie29/main_29.ts
@@ -0,0 +1,22 @@
+
+let favorite_fruits = ["Pineapple","Banana","Mango"];
+
+if(favorite_fruits.includes("Pineapple")){
+    console.log("I Really like Pineapple");
+}
+
+if(favorite_fruits.includes("Banana")){
+    console.log("I Really like Banana");
+}
+
+if(favorite_fruits.includes("Mango")){
+    console.log("I Really like Mango");
+}
+
+if(favorite_fruits.includes("Apple")){
+    console.log("I Really likr Apple");
+}
+
+if(favorite_fruits.includes("Orange")){
+    console.log("I Really like Orange");
+}
