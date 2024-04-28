ae8dfb63d4cdf0c206ca50f544dec2ba8bece829
Atique223 <159752093+Atique223@users.noreply.github.com>
2024-04-28 08:46:48 -0700

    Create main_27.js

---
+++ excersie27/main_27.js
@@ -0,0 +1,32 @@
+var aliencolour = "green";
+if (aliencolour === "green") {
+    console.log("you shot down green alien you have earned 5 point");
+}
+else if (aliencolour === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 point");
+}
+else if (aliencolour === "red") {
+    console.log("you shot down red alien you have earned 15 point");
+}
+// version 2
+var aliencolour2 = "yellow";
+if (aliencolour2 === "green") {
+    console.log("you shot down green alien you have earned 5 point");
+}
+else if (aliencolour2 === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 point");
+}
+else if (aliencolour2 === "red") {
+    console.log("you shot down yellow alien you have earned 15 point");
+}
+// version 3
+var aliencolour3 = "red";
+if (aliencolour3 === "green") {
+    console.log("you shot down green alien you have earned 5 point");
+}
+else if (aliencolour3 === "yellow") {
+    console.log("you shot down yellow alien you have earned 10 point");
+}
+else if (aliencolour3 === "red") {
+    console.log("you shot down yellow alien you have earned 15 point");
+}
